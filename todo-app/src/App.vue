09055d5fcaa992fdacbbd94467287825e66223c6
alesha <work04092017@gmail.com>
2017-11-04 16:11:41 +0300

    Looping and Rendering Data

---
+++ todo-app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class='content'>
+      <todo-list v-bind:todos="todos"></todo-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+</script>
